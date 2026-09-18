cab96d2a4aa1811fe22e6d1d1dacf238e1224071
qqqqq7666 <rudcjs335@gmail.com>
2026-09-18 09:45:36 +0900

    init

---
+++ src/main/java/com/jkc/mydesk/MyDeskApplication.java
@@ -0,0 +1,13 @@
+package com.jkc.mydesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkc/mydesk/MyDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkc.mydesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
